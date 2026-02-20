ca857d83672afe285463e1abbf8dcd715680c4bd
an5onc <cord3576@bears.unco.edu>
2026-02-20 13:41:14 -0700

    nothing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,16 @@
 export default function Home() {
   return (
+<<<<<<< Updated upstream
     <main class="min-h-screen bg-white">
     </main>
+=======
+    <main
+      style={{
+        margin: "0",
+        "min-height": "100vh",
+        background: "#ffffff",
+      }}
+    />
+>>>>>>> Stashed changes
   );
 }
